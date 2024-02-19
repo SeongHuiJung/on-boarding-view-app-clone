885933e1e0a580ed1b8c1567631b184db1cf4bf1
Jseong <tjdgml150237@naver.com>
2024-02-19 17:46:57 +0900

    Initial Commit

---
+++ OnBoardingViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnBoardingViewApp
+//
+//  Created by 정성희 on 2024/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
